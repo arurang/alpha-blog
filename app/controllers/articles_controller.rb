e67e6a1e32050bfdee094a99ce2b8d4b455c8779
Rails Udemy Course <railsudemycourse@gmail.com>
2015-11-28 05:04:24 +0000

    new pages created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
 
+  def index
+@articles = Article.all
+  end
+
+
+
   def new
     @article = Article.new
 
@@ -24,6 +30,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def update
+     @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Aritcle wa successfully updatyed"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
 
 
   private
