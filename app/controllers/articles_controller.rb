f2af858ca73c2d39704ab12e9ef3ecfd8e6c274e
Rails Udemy Course <railsudemycourse@gmail.com>
2015-11-28 06:11:28 +0000

    extrat the redundany cleanup codes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
 @articles = Article.all
@@ -8,32 +9,26 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
-
   end
 
 
   def create
-
-    @article = Article.new(article_params)
       if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
-
-
-
   end
 
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -41,12 +36,12 @@ class ArticlesController < ApplicationController
 
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
 
   def update
-     @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Aritcle wa successfully updatyed"
       redirect_to article_path(@article)
@@ -64,5 +59,8 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 end
