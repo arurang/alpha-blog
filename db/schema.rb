53ed5d37e23a75a6b5748ea854168b4c9e77496b
Rails Udemy Course <railsudemycourse@gmail.com>
2015-11-30 20:37:36 +0000

    add asscoiaction user and articles and debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129135244) do
+ActiveRecord::Schema.define(version: 20151129230554) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "upadted_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
