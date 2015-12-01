380cdca24bd89d8bacf82e4fae0254840527aeff
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-01 11:35:14 +0000

    Update edit and new forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,23 @@ def create
   end
 end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success]="Your Account was updateed successfully "
+      redirect_to articles_path
+
+      else
+      render 'edit'
+    end
+
+
+  end
+
 
   private
 
