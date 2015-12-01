5ef3e40b05f124629b3e47bd2dd6933ab74a6ff4
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-01 20:38:38 +0000

    move to another account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,12 @@ end
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
 
 
