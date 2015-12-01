90f44f36f401637951e731b2dd1c57f458381d6d
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-01 04:33:31 +0000

    sign up user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129230554) do
+ActiveRecord::Schema.define(version: 20151201002316) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
-    t.datetime "upadted_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
   end
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151129230554) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
