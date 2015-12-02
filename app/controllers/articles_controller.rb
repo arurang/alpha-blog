8e9a374a39d844a048f183c0c551355e1a3e3c28
John Smith <aroorang@easy.com>
2015-12-02 06:49:32 +0000

    user_id change to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
 
       @article = Article.new(article_params)
-      @article.user_id = current_user
+      @article.user = current_user
       if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
